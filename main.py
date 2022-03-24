bdbf356c43a5f151da052e9b443d20c91659ce5f
Dre76 <101271612+Dre76@users.noreply.github.com>
2022-03-24 13:48:26 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,47 @@
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line
+
+from doctest import REPORT_CDIFF
+
+
+scorer_name_one = 'Ruud Gullit'
+scorer_name_two = 'Marco van Basten'
+goal_0 = 32
+goal_1 = 54
+
+scorers = 'Ruud Gullit 32, Marco van Basten 54' 
+
+scorer_name_one = 'Ruud Gullit'
+scorer_name_two = 'Marco van Basten'
+when_they_scored = 32
+when_they_scored_to = 54
+
+scored_name = "Ruud Gullit"
+scored_name_1 = "Marco van Basten"
+goal_0 = 32
+goal_1 = 54
+
+f'{scored_name} scored in the {goal_0}\nd minute.' 
+f'{scored_name_1} scored in the {goal_1}\th minute.'
+
+report = f'{scored_name} scored in the {goal_0}nd minute\n' f'{scored_name_1} scored in the {goal_1}th minute'
+
+
+
+player = 'Ruud Gullit'
+first_name = 'Ruud'
+last_name_len = 6
+name_short = 'R. Gullit'
+chant = 'Ruud! Ruud! Ruud! Ruud!'
+good_chant = True
+print(2 != 3)
+print(2 != 2)
+
+
+
+
+
+
